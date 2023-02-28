11cf75f736f0d84116490588074dfe617f6b954f
alexander <alexander1111981@ya.ru>
2023-02-28 14:40:15 +0300

    вариант getState

--- src/pages/services/queryApi.js
+++ src/pages/services/queryApi.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useSelector } from 'react-redux';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 // eslint-disable-next-line import/no-cycle
-//import { store } from '../../store/store';
-import { AccessTokenSelector } from '../../store/selectors/selectors';
+// import { store } from '../../store/store';
+// import { AccessTokenSelector } from '../../store/selectors/selectors';
 import BASE_URL from '../../Base_URL';
 
 // const AccessToken = useSelector(AccessTokenSelector);
@@ -13,9 +13,9 @@ export const spotyfyQueryApi = createApi({
   reducerPath: 'spotyfyApi',
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
-    prepareHeaders: (headers) => {
-      // const { accessToken } = getState().spotyfy;
-      const accessToken = useSelector(AccessTokenSelector);
+    prepareHeaders: (headers, { getState }) => {
+      const { accessToken } = getState().spotyfy;
+      // const accessToken = useSelector(AccessTokenSelector);
       if (accessToken) {
         headers.set('authorizathion', `Bearer${accessToken}`);
       }
